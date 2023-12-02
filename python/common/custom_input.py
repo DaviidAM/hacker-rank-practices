2b9c1c471df10b6a49ff8be2cf96cae241af88a4
David <david.alba@vodafone.com>
2023-12-02 20:29:46 +0000

    nested_list: Working as module

--- python/common/custom_input.py
+++ python/common/custom_input.py
@@ -3,5 +3,6 @@ ITERATION: int = 0
 
 def input2 (test_list:list):
     global ITERATION
-    yield test_list[ITERATION]
+    value = test_list[ITERATION]
     ITERATION += 1
+    return value
--- python/nested_list/my_solution.py
+++ python/nested_list/my_solution.py
@@ -1,9 +1,12 @@
-
 from ..common.custom_input import input2
 from .tests import TEST1
 
+# python3 -m python.nested_list.my_solution
+
+TEST = TEST1
+
 if __name__ == '__main__':
-    for _ in range(int(input2(TEST1))):
-        name = input2(TEST1)
-        score = float(input2(TEST1))
+    for _ in range(int(input2(TEST))):
+        name = input2(TEST)
+        score = float(input2(TEST))
         print(name, score)
