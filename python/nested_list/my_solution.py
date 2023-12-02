ef9a386de549ba978cfb5dacb9be07b730b32a65
David <david.alba@vodafone.com>
2023-12-02 22:47:49 +0000

    nested list exercise completed

--- python/nested_list/my_solution.py
+++ python/nested_list/my_solution.py
@@ -1,12 +1,45 @@
+from copy import deepcopy
 from ..common.custom_input import input2
-from .tests import TEST1
+from .tests import TEST1, TEST8
 
 # python3 -m python.nested_list.my_solution
 
 TEST = TEST1
 
+class ScoreNames():
+
+    score: float
+    names:list[str]
+
+    def __init__(self):
+        # Assuming max note is 100
+        self.score = 100
+        self.names = []
+
 if __name__ == '__main__':
+    lowest = ScoreNames()
+    second = ScoreNames()
     for _ in range(int(input2(TEST))):
+        
+        # Read values
         name = input2(TEST)
         score = float(input2(TEST))
-        print(name, score)
+        
+        # Solution
+        if score < lowest.score:
+            second = deepcopy(lowest)
+            lowest.score = score
+            lowest.names = [name]
+        elif score == lowest.score:
+            lowest.names.append(name)
+        else:
+            if score < second.score:
+                second.score = score
+                second.names = [name]
+            elif score == second.score:
+                second.names.append(name)
+
+    for answer in sorted(second.names):
+        print(answer)
+
+
